640e1d34c4c7a89e73f609ee8fcb86d1f1f85699
Albert <67117216+Albert8419@users.noreply.github.com>
2024-03-21 14:02:25 +0000

    1

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://ubiquitous-fishstick-xxv9q5r9j543x9w-3000.app.github.dev/";
+const API_URL = "https://ubiquitous-fishstick-xxv9q5r9j543x9w-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
